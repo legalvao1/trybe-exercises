56f864f1b7874e8a186d6c348153bbe49271677c
legalvao1 <legalvao1@gmail.com>
2021-03-05 19:15:43 -0300

    Adicionando os dias do mes

--- exercises/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/bloco_5/javascript-dom-eventos-e-web-storage_3/script.js
@@ -14,3 +14,20 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  // Exercício 1
+
+  function creatDaysOfMonth(){
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const dezDays = document.querySelector('#days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1){
+      const days = dezDaysList[index];
+      const dezDaysListItem = document.createElement('li')
+      dezDaysListItem.id = 'day'
+      dezDaysListItem.innerHTML = days;
+
+      dezDays.appendChild(dezDaysListItem);
+    };
+  };
+
+  creatDaysOfMonth();
