4ec776e4122196064d00b014bb41f449c8b9196f
legalvao1 <legalvao1@gmail.com>
2021-03-23 16:15:22 -0300

    Commit inicial do bloco 7.1

---
+++ Fundamentos do Desenvolvimento Web/bloco_7/introducao-a-javascript-es6-e-testes-unitarios.1/conteúdo/pate1VarConstLet.js
@@ -0,0 +1,23 @@
+const favoriteLanguage = "Javascript";
+favoriteLanguage = "Python";
+console.log(favoriteLanguage); // Erro
+
+let favoriteTechnology = "Machine learning";
+favoriteTechnology = "Facial recognition";
+console.log(favoriteTechnology); // Facial recognition
+
+const userInfo = {
+    name: "Cláudio",
+    id: "5489-2",
+    email: "claudio@email.com"
+  };
+  userInfo.name = "João"
+  
+  console.log(userInfo) // { name: "João", id: "5483-2", email: "claudio@email.com }
+
+const technologies = ['Javascript', 'CSS', 'HTML'];
+technologies.push('React');
+console.log(technologies); // [ 'Javascript', 'CSS', 'HTML', 'React' ]
+
+technologies = ['Javascript', 'CSS', 'HTML', 'React']
+console.log(technologies); // Erro
