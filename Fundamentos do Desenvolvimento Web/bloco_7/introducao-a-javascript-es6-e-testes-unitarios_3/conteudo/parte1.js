1d33d90e1e824841849ed61f280f0ba65add1acc
legalvao1 <legalvao1@gmail.com>
2021-03-26 13:13:24 -0300

    Exercicios 1 ao 4

--- Fundamentos do Desenvolvimento Web/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_3/conteudo/parte1.js
+++ Fundamentos do Desenvolvimento Web/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_3/conteudo/parte1.js
@@ -67,3 +67,12 @@ function division(x, y) {
 
 assert.strictEqual(division(10, 2), 5); // OK
 assert.throws(() => { division(10, 0); }, /^Error: parameter y must not be 0$/); // OK
+
+function division(x, y) {
+    // Queremos que o código retorne um erro com uma mensagem específica
+    // caso o parâmetro y seja 0. Por isso lançamos uma exceção se essa condição
+    // for verdadeira, o que irá interromper a execução da função.
+  
+    if (y === 0) throw new Error('parameter y must not be 0');
+    return x / y;
+  }
