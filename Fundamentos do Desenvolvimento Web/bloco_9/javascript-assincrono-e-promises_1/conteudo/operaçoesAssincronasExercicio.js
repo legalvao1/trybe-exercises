b3623650f84baecd3b219ac5c2f4461c86d2de41
legalvao1 <legalvao1@gmail.com>
2021-04-12 18:31:14 -0300

    Adicionando explicaçao ao conteudo operaçoes assincronas exercicio

--- Fundamentos do Desenvolvimento Web/bloco_9/javascript-assincrono-e-promises_1/conteudo/operaçoesAssincronasExercicio.js
+++ Fundamentos do Desenvolvimento Web/bloco_9/javascript-assincrono-e-promises_1/conteudo/operaçoesAssincronasExercicio.js
@@ -24,3 +24,20 @@ pushNumber(numbers, 2);
 pushNumber(numbers, 3);
 
 assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
+
+// TERCEIRO TESTE - Para passar no teste, é necessário testar o numbers após 3000 milissegundos:
+
+const assert = require('assert');
+
+const pushNumber = (list, number) => {
+  list.push(number);
+  console.log(list);
+};
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
