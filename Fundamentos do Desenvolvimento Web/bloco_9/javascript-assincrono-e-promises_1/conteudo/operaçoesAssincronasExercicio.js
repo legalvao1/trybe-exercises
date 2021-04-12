bb5b421d25212f2f7aac2ad523e116a164ac0348
legalvao1 <legalvao1@gmail.com>
2021-04-12 18:13:27 -0300

    Bloco 9.1 - JavaScript Assíncrono e Promises

---
+++ Fundamentos do Desenvolvimento Web/bloco_9/javascript-assincrono-e-promises_1/conteudo/operaçoesAssincronasExercicio.js
@@ -0,0 +1,26 @@
+// Primeiro teste
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]);
+
+// Segundo teste
+
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
