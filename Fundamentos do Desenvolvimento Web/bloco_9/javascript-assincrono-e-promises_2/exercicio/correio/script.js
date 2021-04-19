fe1de071cface5e92c73a16e8c5261abd2e0d81f
legalvao1 <legalvao1@gmail.com>
2021-04-19 15:02:26 -0300

    Exercicios extra bloco 9 - requisiçao com api

--- Fundamentos do Desenvolvimento Web/bloco_9/javascript-assincrono-e-promises_2/exercicio/correio/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_9/javascript-assincrono-e-promises_2/exercicio/correio/script.js
@@ -1,12 +1,35 @@
 const form = document.querySelector('form')
 const input = document.querySelector('input');
+const textoCep = document.querySelector('.cep-result');
 
+const creatAdress = ({ bairro, cep, logradouro, uf, localidade }) => {
+  const tbody = document.querySelector('tbody');
+  const tr = document.createElement('tr');
+  const address = document.createElement('td');
+  const bairroDistrito = document.createElement('td');
+  const city = document.createElement('td');
+  const UF = document.createElement('td');
+  const CEP = document.createElement('td');
+  address.innerHTML = logradouro;
+  bairroDistrito.innerHTML = bairro;
+  city.innerHTML = localidade;
+  UF.innerHTML = uf;
+  CEP.innerHTML = cep;
+
+  tr.appendChild(address);
+  tr.appendChild(bairroDistrito);
+  tr.appendChild(city);
+  tr.appendChild(UF);
+  tr.appendChild(CEP);
+  tbody.appendChild(tr);
+  
+}
 
 const fetchCep = (cep) => {
   fetch(`https://viacep.com.br/ws/${cep}/json/`)
   .then(result => result.json())
-  .then(address => alert(address.bairro))
-  .catch(error => alert('Não foi possível se conectar com a API '));
+  .then(address => creatAdress(address))
+  .catch(error => alert('Informe o CEP'));
 }
 
 const recuperaCep = (event) => {
