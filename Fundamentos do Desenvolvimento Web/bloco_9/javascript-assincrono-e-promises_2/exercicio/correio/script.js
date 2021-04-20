cdcdc21957c6ec8a305dfb7df84ca98a25339f1d
legalvao1 <legalvao1@gmail.com>
2021-04-20 10:21:20 -0300

    Escondendo o busca cep e adicionando botao de nova busca

--- Fundamentos do Desenvolvimento Web/bloco_9/javascript-assincrono-e-promises_2/exercicio/correio/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_9/javascript-assincrono-e-promises_2/exercicio/correio/script.js
@@ -1,27 +1,30 @@
 const form = document.querySelector('form')
 const input = document.querySelector('input');
-const textoCep = document.querySelector('.cep-result');
+const serachCepContainer = document.querySelector('.search-cep-container')
+const tbody = document.querySelector('tbody');
+const table = document.getElementsByTagName('table');
+const tr = document.createElement('tr');
+const newSearchButton = document.querySelector('.new-search-button')
+
 
 //http://diego-cipriano.blogspot.com/2010/09/mascaras-javascript-data-cep-cpf-cnpj.html
 
 const creatAdress = ({ bairro, cep, logradouro, uf, localidade }) => {
-  const tbody = document.querySelector('tbody');
-  const table = document.getElementsByTagName('table');
-  table[0].className = "cep-result table table-primary"
-  const tr = document.createElement('tr');
   const address = document.createElement('td');
   const bairroDistrito = document.createElement('td');
   const city = document.createElement('td');
   const UF = document.createElement('td');
   const CEP = document.createElement('td');
+  table[0].className = "cep-result table table-primary"
   address.innerHTML = logradouro;
   address.className = "coluna-logradouro";
   bairroDistrito.innerHTML = bairro;
   bairroDistrito.className = "coluna-bairro";
-  city.innerHTML = localidade + "/" + uf;  
+  city.innerHTML = `${localidade}/${uf}`;
   city.className = "coluna-uf"
   CEP.innerHTML = cep;
   CEP.className = "coluna-cep";
+  newSearchButton.classList.remove('esconde');
 
   tr.appendChild(address);
   tr.appendChild(bairroDistrito);
@@ -29,8 +32,9 @@ const creatAdress = ({ bairro, cep, logradouro, uf, localidade }) => {
   tr.appendChild(UF);
   tr.appendChild(CEP);
   tbody.appendChild(tr);
-  
+
 }
+
 
 const fetchCep = (cep) => {
   fetch(`https://viacep.com.br/ws/${cep}/json/`)
@@ -45,4 +49,17 @@ const recuperaCep = (event) => {
   fetchCep(cepRecebido);
 }
 
-form.addEventListener('submit', recuperaCep)
+const hideSearchCep = () => {
+  serachCepContainer.classList.add('esconde');
+}
+
+const backTosearchCep = () => {
+  serachCepContainer.classList.remove('esconde');
+  console.log(serachCepContainer.classList)
+  tbody.remove(tr);
+
+}
+
+form.addEventListener('submit', recuperaCep);
+form.addEventListener('submit', hideSearchCep);
+newSearchButton.addEventListener('click', backTosearchCep)
