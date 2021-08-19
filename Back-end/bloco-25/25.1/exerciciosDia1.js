b41398087f7c919abef547de1e3cd7264efdbd9d
legalvao1 <legalvao1@gmail.com>
2021-08-19 15:17:09 -0300

    Exercicios do dia parte 2

--- Back-end/bloco-25/25.1/exerciciosDia1.js
+++ Back-end/bloco-25/25.1/exerciciosDia1.js
@@ -107,7 +107,7 @@ db.vendas.aggregate([
     $match: { 
       status: { 
         $in: ["ENTREGUE", "EM SEPARACAO"] 
-      } 
+      }
     } 
   },
   {
@@ -120,4 +120,194 @@ db.vendas.aggregate([
   },
   { $sort: { total: -1 } },
   { $limit: 5 }
+]);
+
+//Exercício 9 : Descubra quais são os 10 clientes que gastaram o maior valor no ano de 2019 .
+
+// minha resposta usando apenas os clientes que PAGARAM
+db.vendas.aggregate([
+  { 
+    $match: { 
+      status: { 
+        $in: ["ENTREGUE", "EM SEPARACAO"] 
+      },
+      dataVenda: { $gte: ISODate("2019-01-01"), $lte: ISODate("2019-12-31")}
+    } 
+  },
+  {
+    $group: {
+      _id: "$clienteId",
+      total: { 
+        $sum: "$valorTotal"
+      }
+    }
+  },
+  { $sort: { total: -1 } },
+  { $limit: 10 }
+]);
+
+// gabarito
+db.vendas.aggregate([
+  { 
+    $match: { 
+      dataVenda: { $gte: ISODate("2019-01-01"), $lte: ISODate("2019-12-31")}
+    } 
+  },
+  {
+    $group: {
+      _id: "$clienteId",
+      total: { 
+        $sum: "$valorTotal"
+      }
+    }
+  },
+  { $sort: { total: -1 } },
+  { $limit: 10 }
+]);
+
+//Exercício 10 : Descubra quantos clientes compraram mais de 5 vezes. Retorne um documento que contenha somente o campo clientes com o total de clientes.
+//Dica: O operador $count pode simplificar sua query .
+
+db.vendas.aggregate([
+  {
+    $group: {
+      _id: "$clienteId",
+      total: { 
+        $sum: 1
+      }
+    }
+  },
+  { 
+    $match: { total: { $gte: 5 } }
+  },
+  { $count: "clientes" }
+]);
+
+// sem o count
+
+db.vendas.aggregate([
+  {
+    $group: {
+      _id: "$clienteId",
+      total: { 
+        $sum: 1
+      }
+    }
+  },
+  { 
+    $match: { total: { $gte: 5 } }
+  },
+  {
+    $group: {
+      _id: null,
+      clientes: { $sum: 1 }
+    }
+  },
+  {
+    $project: {
+      _id: 0
+    }
+  }
+]);
+
+//Exercício 11 : Descubra quantos clientes compraram menos de três vezes entre os meses de Janeiro de 2020 e Março de 2020 .
+
+
+db.vendas.aggregate([
+  {
+    $match: {
+      dataVenda: { $gte: ISODate("2020-01-01"), $lte: ISODate("2020-03-31")}
+    }
+  },
+  {
+    $group: {
+      _id: "$clienteId",
+      total: { 
+        $sum: 1
+      }
+    }
+  },
+  { 
+    $match: { total: { $lt: 3 } }
+  },
+  { $count: "clientes" }
+]);
+
+//Exercício 12 : Descubra quais as três uf s que mais compraram no ano de 2020 . Retorne os documentos no seguinte formato:
+
+db.vendas.aggregate([
+  {
+    $match: {
+      dataVenda: { $gte: ISODate("2020-01-01"), $lte: ISODate("2020-12-31")}
+    }
+  },
+  {
+    $lookup: {
+      from: "clientes",
+      localField: "clienteId",
+      foreignField: "clienteId",
+      as: "dadosDoCliente"
+    }
+  },
+  {
+    $unwind: "$dadosDoCliente"
+  },
+  {
+    $group: {
+      _id: "$dadosDoCliente.endereco.uf",
+      totalVendas: { 
+        $sum: 1
+      }
+    }
+  },
+  { $sort: { _id: 1 } },
+  { $limit: 3 },
+  {
+    $project: {
+      _id: 0,
+      uf: "$_id",
+      totalVendas: 1
+    }
+  }
+]);
+
+// Exercício 13 : Encontre qual foi o total de vendas e a média de vendas de cada uf no ano de 2019 . Ordene os resultados pelo nome da uf . Retorne os documentos no seguinte formato:
+
+db.vendas.aggregate([
+  {
+    $match: {
+      dataVenda: { $gte: ISODate("2019-01-01"), $lte: ISODate("2019-12-31")}
+    }
+  },
+  {
+    $lookup: {
+      from: "clientes",
+      localField: "clienteId",
+      foreignField: "clienteId",
+      as: "dadosDoCliente"
+    }
+  },
+  {
+    $unwind: "$dadosDoCliente"
+  },
+  {
+    $group: {
+      _id: "$dadosDoCliente.endereco.uf",
+      totalVendas: { 
+        $sum: 1
+      },
+      mediaVendas: { $avg: "$valorTotal"}
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      uf: "$_id",
+      totalVendas: 1,
+      mediaVendas: 1,
+    }
+  },
+  {
+    $sort: { uf: 1 }
+  }
 ]);
