ec2e2539d114d0d3efd2acc30c9af8bb5a0bc24c
legalvao1 <legalvao1@gmail.com>
2021-09-28 11:12:46 -0300

    Primeiro commit exercicio

---
+++ Back-end/bloco-29/29.1/ex-Deploy/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const server_env = process.env.SERVER_ENV || 'Não Foi :/';
+
+const PORT = process.env.PORT || '3000';
+
+app.get('/', (req, res) => {
+    res.send("Está vivo!!!")
+});
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
