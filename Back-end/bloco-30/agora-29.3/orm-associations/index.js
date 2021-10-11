32e0dd0912b3ecc06aa0933b623c932823c36565
legalvao1 <legalvao1@gmail.com>
2021-10-11 15:19:02 -0300

    29.2 - Interface ORM Sequelize

---
+++ Back-end/bloco-30/agora-29.3/orm-associations/index.js
@@ -0,0 +1,83 @@
+// index.js
+const express = require('express');
+const { Address, Employee } = require('./models');
+const { Book, User } = require('./models');
+
+const app = express();
+
+app.get('/employees', async (_req, res) => {
+  try {
+    const employees = await Employee.findAll({
+      include: { model: Address, as: 'addresses' },
+    });
+
+    return res.status(200).json(employees);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
+
+// app.get('/employees/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const employee = await Employee.findOne({
+//         where: { id },
+//         include: [{ model: Address, as: 'addresses',  attributes: { exclude: ['number'] } }],
+//       });
+
+//     if (!employee)
+//       return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+//     return res.status(200).json(employee);
+//   } catch (e) {
+//     console.log(e.message);
+//     res.status(500).json({ message: 'Algo deu errado' });
+//   };
+// });
+
+
+
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+        const employee = await Employee.findOne({ where: { id } });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+        if (req.query.includeAddresses === 'true') {
+          const addresses = await Address.findAll({ where: { employeeId: id } });
+          return res.status(200).json({ employee, addresses });
+        }
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
+
+module.exports = app;
